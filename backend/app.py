873a365525ca91a6b5229265a7ee93cd7ba7135c
telmapereira3110 <telmapereira7@outlook.com>
2025-04-16 14:56:50 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -47,17 +47,17 @@ app = Flask(__name__)
 CORS(app) # Permite que o React acesse a API
 
 # Variáveis globais para o cache
-cache_dados = {
-    "timestamp_pse": None,
-    "dados_pse": None,
-    "timestamp_wellness": None,
-    "dados_wellness": None,
-    "timestamp_cmj_sj": None,
-    "dados_cmj_sj": None
-}
+# cache_dados = {
+#    "timestamp_pse": None,
+#    "dados_pse": None,
+#    "timestamp_wellness": None,
+#    "dados_wellness": None,
+#    "timestamp_cmj_sj": None,
+#    "dados_cmj_sj": None
+#}
 
 # Tempo de validade do cache (ex: 10 minutos)
-CACHE_TTL = timedelta(minutes=10)
+# CACHE_TTL = timedelta(minutes=10)
 
 # Função para chamada com reintento e espera
 def chamada_com_espera(request, tentativas_max=5, tempo_espera=60):
@@ -113,9 +113,9 @@ def carregar_dados_wellness():
     now = datetime.now()
 
     # Verifica se já temos dados no cache e se estão dentro do TTL
-    if cache_dados["dados_wellness"] is not None and cache_dados["timestamp_wellness"] is not None:
-        if now - cache_dados["timestamp_wellness"] < CACHE_TTL:
-            return cache_dados["dados_wellness"]
+    #if cache_dados["dados_wellness"] is not None and cache_dados["timestamp_wellness"] is not None:
+        #if now - cache_dados["timestamp_wellness"] < CACHE_TTL:
+            #return cache_dados["dados_wellness"]
         
     range_name = 'dados_wellness!A:H'
     spreadsheet_id = '15VCHZTSwrCecWMWCyxJ5PSS5nxXRXsz6rKw_xK5aBxw'
@@ -167,8 +167,8 @@ def carregar_dados_wellness():
         jogadores_unicos = list(dados_wellness.keys())
 
         # Atualiza o cache
-        cache_dados["dados_wellness"] = dados_wellness
-        cache_dados["timestamp_wellness"] = datetime.now()
+        #cache_dados["dados_wellness"] = dados_wellness
+        #cache_dados["timestamp_wellness"] = datetime.now()
 
         print(f"Retornando dados: {dados_wellness} e jogadores: {jogadores_unicos}")  # Adicionando print para depuração
 
@@ -187,9 +187,9 @@ def carregar_dados_pse_carga_treino():
     now = datetime.now()
 
     # Verifica se já temos dados no cache e se estão dentro do TTL
-    if cache_dados["dados_pse"] is not None and cache_dados["timestamp_pse"] is not None:
-        if now - cache_dados["timestamp_pse"] < CACHE_TTL:
-            return cache_dados["dados_pse"]
+    #if cache_dados["dados_pse"] is not None and cache_dados["timestamp_pse"] is not None:
+        #if now - cache_dados["timestamp_pse"] < CACHE_TTL:
+            #return cache_dados["dados_pse"]
         
     range_name ='gpexe_0125!A:Y'
     spreadsheet_id = '1RSBz3OHn7b-G2qD9aRh9Fb69ipUiKIev_wQoSjm4j7k'
@@ -308,8 +308,8 @@ def carregar_dados_pse_carga_treino():
             })
 
             # Atualiza o cache
-        cache_dados["dados_pse"] = ( dados_pse, carga_interna, carga_externa_dt, carga_externa_hs)
-        cache_dados["timestamp_pse"] = datetime.now()
+        #cache_dados["dados_pse"] = ( dados_pse, carga_interna, carga_externa_dt, carga_externa_hs)
+        #cache_dados["timestamp_pse"] = datetime.now()
 
         return dados_pse, carga_interna, carga_externa_dt, carga_externa_hs
     
@@ -571,9 +571,9 @@ def carregar_dados_cmj_sj():
     now = datetime.now()
 
     # Verifica se já temos dados no cache e se estão dentro do TTL
-    if cache_dados["dados_cmj_sj"] is not None and cache_dados["timestamp_cmj_sj"] is not None:
-        if now - cache_dados["timestamp_cmj_sj"] < CACHE_TTL:
-            return cache_dados["dados_cmj_sj"]
+    #if cache_dados["dados_cmj_sj"] is not None and cache_dados["timestamp_cmj_sj"] is not None:
+        #if now - cache_dados["timestamp_cmj_sj"] < CACHE_TTL:
+            #return cache_dados["dados_cmj_sj"]
         
     range_name = 'sj_cmj!A:D'  # Nome correto da aba e colunas
     spreadsheet_id = '1CM-UKcOOWM1LLbBP_OL5nqL7n2IG0MZPQA10WeadQek'
@@ -635,8 +635,8 @@ def carregar_dados_cmj_sj():
             sj[jogador][microciclo] = {"SJ": sj_valor}
 
             # Atualiza o cache
-        cache_dados["dados_cmj_sj"] = (cmj, sj)
-        cache_dados["timestamp_cmj_sj"] = datetime.now()
+        #cache_dados["dados_cmj_sj"] = (cmj, sj)
+        #cache_dados["timestamp_cmj_sj"] = datetime.now()
 
         return cmj, sj
 
@@ -842,15 +842,15 @@ def get_sj(jogador, microciclo):
         return jsonify({"erro": "Jogador ou Microciclo não encontrado"}), 404
     
 # Rota para limpar o cache manualmente
-@app.route('/api/cache/clear', methods=['POST'])
-def clear_cache():
-    cache_dados["dados_pse"] = None
-    cache_dados["timestamp_pse"] = None
-    cache_dados["dados_wellness"] = None
-    cache_dados["timestamp_wellness"] = None
-    cache_dados["dados_cmj_sj"] = None
-    cache_dados["timestamp_cmj_sj"] = None
-    return jsonify({"mensagem": "Cache limpo com sucesso."})
+#@app.route('/api/cache/clear', methods=['POST'])
+#def clear_cache():
+    #cache_dados["dados_pse"] = None
+    #cache_dados["timestamp_pse"] = None
+    #cache_dados["dados_wellness"] = None
+    #cache_dados["timestamp_wellness"] = None
+    #cache_dados["dados_cmj_sj"] = None
+    #cache_dados["timestamp_cmj_sj"] = None
+    #return jsonify({"mensagem": "Cache limpo com sucesso."})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Usar a porta definida pelo Render ou 5000 por padrão
